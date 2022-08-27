e4bbec135700d25816bc63585ac1fe4cb43aebd6
NahidulNoman <nahidulna5@gmail.com>
2022-08-27 12:12:44 +0600

    patch-show-in-brower

--- js/photo.js
+++ js/photo.js
@@ -17,4 +17,17 @@ function addPhotos(){
         }
     })
 }
+function patchAPost() {
+    fetch('https://jsonplaceholder.typicode.com/posts/1', {
+        method: 'PATCH',
+        body: JSON.stringify({
+            title: 'foo',
+        }),
+        headers: {
+            'Content-type': 'application/json; charset=UTF-8',
+        },
+    })
+        .then((response) => response.json())
+        .then((json) => console.log(json));
+}
 addPhotos();
